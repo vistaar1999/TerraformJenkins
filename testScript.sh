8fe58f8a8342734d0a563f83ec6b221a67af6bc5
Vistaar khurana <khuranavistaar@gmail.com>
2023-12-31 15:20:05 +0530

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -5,7 +5,5 @@ RUN_TYPE=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 echo "$RUN_TYPE"
 
 if  [[ $RUN_TYPE == "destroy" ]]; then
-	REAL_RUN="destroy --auto-approve"
-else
-	REAL_RUN=$RUN_TYPE
+	"terraform destroy --auto-approve"
 fi
